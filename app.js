224d0671d709d5b51a20c90591b752b44e08de09
Joel Matthews <joelmatthews.A@gmail.com>
2022-07-13 10:08:47 -0400

    Add basic error handling and began adding basic bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const KayakSpot = require('./models/kayakSpot');
+const ExpressError = require('./utilities/ExpressError');
+const catchAsync = require ('./utilities/catchAsync');
+const ejsMate = require('ejs-mate');
 
 main().catch(err => console.log(err));
 
@@ -16,6 +19,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
@@ -23,45 +27,54 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/kayaking', async (req, res) => {
+app.get('/kayaking', catchAsync(async (req, res) => {
     const kayaking = await KayakSpot.find({});
     res.render('index', { kayaking });
-})
+}))
 
 app.get('/kayaking/new', (req, res) => {
     res.render('new');
 })
 
-app.post('/kayaking', async (req, res) => {
+app.post('/kayaking', catchAsync(async (req, res) => {
     const newKayakSpot = new KayakSpot(req.body.kayak);
     await newKayakSpot.save();
     res.redirect(`/kayaking/${newKayakSpot._id}`)
-})
+}))
 
-app.get('/kayaking/:id', async (req, res) => {
+app.get('/kayaking/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const kayak = await KayakSpot.findById(id);
     res.render('show', { kayak });
-})
+}))
 
-app.get('/kayaking/:id/edit', async (req, res) => {
+app.get('/kayaking/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const foundKayakSpot = await KayakSpot.findById(id);
     res.render('edit', { foundKayakSpot });
-})
+}))
 
-app.put('/kayaking/:id', async (req, res) => {
+app.put('/kayaking/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const updatedKayak = await KayakSpot.findByIdAndUpdate(id, { ...req.body.kayak });
     res.redirect(`/kayaking/${updatedKayak._id}`)
-})
+}))
 
-app.delete('/kayaking/:id', async (req, res) => {
+app.delete('/kayaking/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const deletedKayak = await KayakSpot.findByIdAndDelete(id);
     res.redirect('/kayaking')
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Oh no, page not found', 404));
 })
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh no, something went wrong!'
+    res.status(statusCode).render('error', { err });
+})
 
 
 app.listen(3000, () => {
